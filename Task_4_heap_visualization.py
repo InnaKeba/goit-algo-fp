925ec4497a82587aea90ef3dabb5979da9ddd2fe
InnaKeba <kebainna@gmail.com>
2025-10-02 21:25:58 +0300

    The binary heap function has been implemented. The function visualizes a binary heap.

---
+++ Task_4_heap_visualization.py
@@ -0,0 +1,52 @@
+import uuid
+import networkx as nx
+import matplotlib.pyplot as plt
+
+class Node:
+    def __init__(self, key, color="skyblue"):
+        self.left = None
+        self.right = None
+        self.val = key
+        self.color = color
+        self.id = str(uuid.uuid4())
+
+def build_heap_tree(heap_list, index=0):
+    if index >= len(heap_list):
+        return None
+    node = Node(heap_list[index])
+    node.left = build_heap_tree(heap_list, 2 * index + 1)
+    node.right = build_heap_tree(heap_list, 2 * index + 2)
+    return node
+
+def add_edges(graph, node, pos, x=0, y=0, layer=1):
+    if node is not None:
+        graph.add_node(node.id, color=node.color, label=node.val)
+        if node.left:
+            graph.add_edge(node.id, node.left.id)
+            l = x - 1 / 2 ** layer
+            pos[node.left.id] = (l, y - 1)
+            add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
+        if node.right:
+            graph.add_edge(node.id, node.right.id)
+            r = x + 1 / 2 ** layer
+            pos[node.right.id] = (r, y - 1)
+            add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
+    return graph
+
+def draw_heap(heap_list):
+    root = build_heap_tree(heap_list)
+    tree = nx.DiGraph()
+    pos = {root.id: (0, 0)}
+    tree = add_edges(tree, root, pos)
+
+    colors = [node[1]['color'] for node in tree.nodes(data=True)]
+    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}
+
+    plt.figure(figsize=(8, 5))
+    nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
+    plt.title("Візуалізація бінарної купи")
+    plt.show()
+
+heap = [0, 4, 1, 5, 10, 3]
+
+draw_heap(heap)
